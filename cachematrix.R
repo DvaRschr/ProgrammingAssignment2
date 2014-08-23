c831b78406f3c4ddeb1280149d88dd02230da115
dvarschr <ctabak@cornellcollege.edu>
2014-08-23 17:38:53 -0500

    Started Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment No. 2 is going to involve 
+## the use of the "solve" funtion.
 
 ## Write a short comment describing this function
 
